0c632403992fc119164ca2812921741963aa8ce4
Brandon-Cave <brandon.c.cave@outlook.com>
2025-03-19 14:01:27 -0700

    Deleted jose.ps1 and modified MoveLondonUsers to take user inputs and count the total moved users.

--- MoveLondonUsers.ps1
+++ MoveLondonUsers.ps1
@@ -1,12 +1,12 @@
-$OUName     = "London"
-$DomainDN   = "DC=Adatum,DC=com"
+$OUName     = Read-Host "Enter the name of the OU"
+$DomainDN   = Read-Host "Enter the Domain Distinguished Name (e.g., DC=Adatum,DC=com)"
 $OUPath     = "OU=$OUName,$DomainDN"
-$GroupName  = "London Users"
-$LondonUsers = Get-ADUser -Filter {City -eq $OUName} -Properties City 
-
-foreach ($user in $LondonUsers)
+$GroupName  = Read-Host "Enter the name of the group"
+$MovingUsers = (Get-ADUser -Filter {City -eq $OUName} -Properties City)
+$UserCount = $MovingUsers.Count
+foreach ($user in $MovingUsers)
 {
     Move-ADObject -Identity $user -TargetPath $OUPath
     Add-ADGroupMember -Identity $GroupName -Members $user
 }
-Write-Host "All users in the '$OUName' OU have been moved to the '$OUName' OU and added to the $GroupName security group."
+Write-Host "All '$UserCount' users with '$OUName' as their city have been moved to the '$OUName' OU and added to the $GroupName security group."
